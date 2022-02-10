4380ef462a72ceec4b5bfe2494882744bdd35f58
johannes545 <Johannes545@gmail.com>
2022-02-10 21:25:23 +0100

    finished the assignment

--- 10_02/10_02/Program.cs
+++ 10_02/10_02/Program.cs
@@ -6,7 +6,24 @@ namespace _10_02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); 
+
+
+            string cat = "Kitty";
+            Console.WriteLine(cat);
+            int catAge = 18;
+            Console.WriteLine(catAge);
+            char catChar = 'C';
+            Console.WriteLine(catChar);
+            bool catBool = true;
+            Console.WriteLine(catBool);
+            double catDouble = 7.77;
+            Console.WriteLine(catDouble);
+            decimal catDecimal = 777777m;
+            Console.WriteLine(catDecimal);
+            Console.WriteLine($"{ cat},{ catAge} etc etc, just continue to add the references in curly braces.");
+
+
         }
     }
 }
